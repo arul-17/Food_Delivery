1f04009d304606899793b2b65877a93432198da5
Aruljeevika <94167729+arul-17@users.noreply.github.com>
2025-03-25 17:17:23 +0530

    Update FoodApplicationTests.java

--- Food_Delivery/src/test/java/com/food/FoodApplicationTests.java
+++ Food_Delivery/src/test/java/com/food/FoodApplicationTests.java
@@ -1,1 +1,13 @@
-file
+package com.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
